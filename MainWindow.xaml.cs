6abf3df2b46a184183352c6f0d9499c99561e730
leetankhongs <55917426+leetankhongs@users.noreply.github.com>
2019-12-27 22:46:10 +0700

    commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Gma.System.MouseKeyHook;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Timers;
 using System.Windows;
 using System.Windows.Input;
@@ -21,7 +23,12 @@ namespace WpfApp_Windows_Project3_MultimediaPlayer
         int _lastIndex = -1;
         bool _isPlaying = false;
         bool volumeOff = false;
+        bool random = false;
+        bool repeated = false;
+        double oldValueVolume;
+        int repeat = 0;
         Timer timer;
+        private IKeyboardMouseEvents _hook;
         
         public MainWindow()
         {
@@ -31,7 +38,11 @@ namespace WpfApp_Windows_Project3_MultimediaPlayer
             timer.Interval = 1000;
             timer.Elapsed += timer_Elapsed;
             timer.Start();
+
+            _hook = Hook.GlobalEvents();
+            _hook.KeyUp += keyUp_hook;
         }
+
         private void timer_Elapsed(object sender, ElapsedEventArgs e)
         {
 
@@ -50,37 +61,116 @@ namespace WpfApp_Windows_Project3_MultimediaPlayer
 
         private void _player_MediaEnded(object sender, EventArgs e)
         {
-            _lastIndex++;
+            if(random == false)
+            {
+                _lastIndex++;
+                if (_lastIndex == _fullPaths.Count())
+                {
+                    if (repeat == 0)
+                    {
+
+                        _lastIndex = -1;
+                        return;
+
+                    }
+                    else
+                    {
+                        if (repeat == 1)
+                        {
+                            _lastIndex = 0;
+                        }
+                        else
+                        {
+                            if (repeated == false)
+                            {
+                                repeated = true;
+                                _lastIndex = 0;
+                            }
+                            else
+                            {
+                                _lastIndex = -1;
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (positionNotPlayedYet.Count() == 0)
+                {
+                    _lastIndex = -1;
+
+                    if (repeat == 0)
+                    {
+                        _lastIndex = -1;
+                        return;
+                    }
+                    else
+                    {
+                        if (repeat == 1)
+                        {
+                            positionNotPlayedYet.Clear();
+                            for (int i = 0; i < _fullPaths.Count(); i++)
+                                positionNotPlayedYet.Add(i);
+                        }
+                        else
+                        {
+                            if (repeated == false)
+                            {
+                                repeated = true;
+                                positionNotPlayedYet.Clear();
+                                for (int i = 0; i < _fullPaths.Count(); i++)
+                                    positionNotPlayedYet.Add(i);
+                            }
+                            else
+                            {
+                                _lastIndex = -1;
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                Random rnd = new Random();
+                int position = rnd.Next(0, positionNotPlayedYet.Count() - 1);
+                _lastIndex = positionNotPlayedYet[position];
+                positionNotPlayedYet.RemoveAt(position);
+
+            }
+
             PlaySelectedIndex(_lastIndex);
         }
 
         private void PlaySelectedIndex(int i)
         {
+            if (_isPlaying == false)
+                return; 
 
             string filename = _fullPaths[i].FullName;
-
             _player.Open(new Uri(filename, UriKind.Absolute));
-            System.Threading.Thread.Sleep(500);
             _player.Play();
+            System.Threading.Thread.Sleep(800);
             _isPlaying = true;
+            TimeSlider.Maximum = _player.NaturalDuration.TimeSpan.TotalSeconds;
+
         }
 
-
         BindingList<FileInfo> _fullPaths = new BindingList<FileInfo>();
+        List<int> positionNotPlayedYet = new List<int>();
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
             playlistListBox.ItemsSource = _fullPaths;
             _player.Volume = 1;
             volumeSlider.Value = volumeSlider.Maximum;
             volumeOff = true;
-
         }
 
         private void Browserbtn_Click(object sender, RoutedEventArgs e)
         {
             var screen = new Microsoft.Win32.OpenFileDialog();
+
             if (screen.ShowDialog() == true)
             {
                 var info = new FileInfo(screen.FileName);
@@ -89,23 +179,40 @@ namespace WpfApp_Windows_Project3_MultimediaPlayer
         }
         private void playlistListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-       
+            _isPlaying = true;
+
             if (playlistListBox.SelectedIndex >= 0)
             {
+                if (random == true)
+                {
+                    positionNotPlayedYet.Clear();
+                    for (int i = 0; i < _fullPaths.Count(); i++)
+                        positionNotPlayedYet.Add(i);
+
+                    positionNotPlayedYet.Remove(playlistListBox.SelectedIndex);
+                }
+
+                if(repeat == 2)
+                    repeated = false;
+
                 _lastIndex = playlistListBox.SelectedIndex;
                 PlaySelectedIndex(_lastIndex);
-                TimeSlider.Maximum = _player.NaturalDuration.TimeSpan.TotalSeconds;
-
-                currentPostTblock.Text = _player.Position.ToString(@"mm\:ss");
-                durationTblock.Text = _player.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
-                _isPlaying = true;
+                showUI();
             }
             else
             {
                MessageBox.Show("No file selected!");
             }
         }
+        private void showUI()
+        {
+            if (_isPlaying == false)
+                return;
 
+            currentPostTblock.Text = _player.Position.ToString(@"mm\:ss");
+            durationTblock.Text = _player.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
+            _isPlaying = true;
+        }
         private void TimeSlider_PreviewMouseUp(object sender, MouseButtonEventArgs e)
         {
             _player.Position = TimeSpan.FromSeconds(TimeSlider.Value);
@@ -115,6 +222,12 @@ namespace WpfApp_Windows_Project3_MultimediaPlayer
         {
             if (playlistListBox.SelectedIndex < 0)
                 return;
+
+            playOrPauseSong();
+        }
+
+        private void playOrPauseSong()
+        {
 
             if (_isPlaying == true)
             {
@@ -126,10 +239,9 @@ namespace WpfApp_Windows_Project3_MultimediaPlayer
             {
                 _player.Play();
                 Playimg.Source = new BitmapImage(new Uri("Images/pause.png", UriKind.Relative));
-                _isPlaying = true ;
+                _isPlaying = true;
             }
         }
-
         private void volumeSlider_PreviewMouseUp(object sender, MouseButtonEventArgs e)
         {
             _player.Volume = volumeSlider.Value;
@@ -149,7 +261,6 @@ namespace WpfApp_Windows_Project3_MultimediaPlayer
 
         }
 
-        double oldValueVolume;
         private void volumeButton_Click(object sender, RoutedEventArgs e)
         {
             if (volumeOff == true)
@@ -167,6 +278,100 @@ namespace WpfApp_Windows_Project3_MultimediaPlayer
                 volumeImg.Source = new BitmapImage(new Uri("Images/volumeoff.png", UriKind.Relative));
                 volumeOff = true;
 
+            }
+        }
+
+        private void keyUp_hook(object sneder, System.Windows.Forms.KeyEventArgs e)
+        {
+            if(e.Control && e.Shift && (e.KeyCode == System.Windows.Forms.Keys.S))
+                playOrPauseSong();
+
+
+            if (e.Control && e.Shift && (e.KeyCode == System.Windows.Forms.Keys.A))
+                playPreviousSong();
+
+            if (e.Control && e.Shift && (e.KeyCode == System.Windows.Forms.Keys.D))
+                playNextSong();
+
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            _hook.KeyUp -= keyUp_hook;
+            _hook.Dispose();
+        }
+
+        private void playNextSong()
+        {
+            _lastIndex++;
+            if (_lastIndex >= _fullPaths.Count())
+                _lastIndex = 0;
+            PlaySelectedIndex(_lastIndex);
+            showUI();
+        }
+        private void playPreviousSong()
+        {
+            _lastIndex--;
+
+            if (_lastIndex < 0)
+                _lastIndex = _fullPaths.Count() - 1;
+
+            PlaySelectedIndex(_lastIndex);
+            showUI();
+        }
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            playNextSong();
+        }
+
+        private void Previousbtn_Click(object sender, RoutedEventArgs e)
+        {
+            playPreviousSong();
+        }
+
+        private void Random_Click(object sender, RoutedEventArgs e)
+        {
+            if (random == false)
+            {
+                RandomEllipse.Fill = new SolidColorBrush(Colors.Red);
+                random = true;
+                positionNotPlayedYet.Clear();
+
+                for (int i = 0; i < _fullPaths.Count(); i++)
+                    positionNotPlayedYet.Add(i);
+
+                if (_lastIndex >= 0)
+                    positionNotPlayedYet.Remove(_lastIndex);
+            }
+            else
+            {
+                RandomEllipse.Fill = new SolidColorBrush(Colors.Transparent);
+                random = false;
+            }
+
+        }
+
+        private void repeatbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(repeat == 0)
+            {
+                repeatEllipse.Fill = new SolidColorBrush(Colors.Red);
+                repeat = 1;
+                return;
+            }
+
+            if(repeat == 1)
+            {
+                repeatImage.Source = new BitmapImage(new Uri("Images/repeat1.png", UriKind.Relative));
+                repeat = 2;
+                return;
+            }
+
+            if(repeat == 2)
+            {
+                repeatEllipse.Fill = new SolidColorBrush(Colors.Transparent);
+                repeatImage.Source = new BitmapImage(new Uri("Images/repeat.png", UriKind.Relative));
+                repeat = 0;
+                return;
             }
         }
     }
